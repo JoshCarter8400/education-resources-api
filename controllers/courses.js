63149ecf160949714c0b2a3c9b5c43e3b8303bf8
Josh Carter <joshcarter8400@gmail.com>
2021-02-20 14:00:23 -0500

    added in creating a course for specific bootcamp and getting a single course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc Get Courses
 // @route Get /api/v1/courses
@@ -20,4 +21,42 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   const courses = await query;
 
   res.status(200).json({ success: true, count: courses.length, data: courses });
+});
+
+// @desc Get Single Course
+// @route Get /api/v1/courses/:id
+//access Public no token needed
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course found with Id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({ success: true, data: course });
+});
+
+// @desc Create a Course
+// @route Post /api/v1/bootcamps/:bootcampId/courses
+//access Private token needed
+exports.createCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `No bootcamp found with Id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({ success: true, data: course });
 });
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const {
   getCourses,
-  //   getCourse,
+  getCourse,
   //   updateCourse,
-  //   createCourse,
+  createCourse,
   //   deleteCourse,
   //   getCoursesInRadius,
 } = require('../controllers/courses');
@@ -11,8 +11,8 @@ const router = express.Router({ mergeParams: true });
 
 // router.route('/radius/:zipcode/:distance').get(getCoursesInRadius);
 
-router.route('/').get(getCourses);
-// .post(createCourse);
-// router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+router.route('/').get(getCourses).post(createCourse);
 
+router.route('/:id').get(getCourse);
+// .put(updateCourse).delete(deleteCourse);
 module.exports = router;
