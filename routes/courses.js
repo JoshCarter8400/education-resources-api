8634d5220d33c3fd0e3dedcbb2067746cf724f28
Josh Carter <joshcarter8400@gmail.com>
2021-02-20 13:10:22 -0500

    set up course controller and initila route

---
+++ routes/courses.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {
+  getCourses,
+  //   getCourse,
+  //   updateCourse,
+  //   createCourse,
+  //   deleteCourse,
+  //   getCoursesInRadius,
+} = require('../controllers/courses');
+const router = express.Router({ mergeParams: true });
+
+// router.route('/radius/:zipcode/:distance').get(getCoursesInRadius);
+
+router.route('/').get(getCourses);
+// .post(createCourse);
+// router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+
+module.exports = router;
