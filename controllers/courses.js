d80df54b53c2ad33bda3541600c4c4bb427f3527
Josh Carter <joshcarter8400@gmail.com>
2021-02-21 16:11:24 -0500

    only owners can update and delete courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -43,6 +43,8 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 //access Private token needed
 exports.createCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
@@ -50,6 +52,16 @@ exports.createCourse = asyncHandler(async (req, res, next) => {
       new ErrorResponse(
         `No bootcamp found with Id of ${req.params.bootcampId}`,
         404
+      )
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${user.params.id} is not authorized to add a course to this ${bootcamp._id}`,
+        401
       )
     );
   }
@@ -70,6 +82,17 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
       new ErrorResponse(`No course found with Id of ${req.params.id}`, 404)
     );
   }
+
+  // Make sure user is bootcamp owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete course ${course._id}`,
+        401
+      )
+    );
+  }
+
   await course.remove();
   res.status(200).json({ success: true, data: course });
 });
@@ -83,6 +106,16 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(
       new ErrorResponse(`No course found with Id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add update course ${course._id}`,
+        401
+      )
     );
   }
 
