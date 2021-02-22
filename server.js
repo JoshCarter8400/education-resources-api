909e22dae8f9f74768161dd339047bcd10e737f0
Josh Carter <joshcarter8400@gmail.com>
2021-02-22 13:42:21 -0500

    added CRUD functionality for users and set it to admin only

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 const app = express();
 
 // Body Parser
@@ -40,6 +41,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
